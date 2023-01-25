5df74c9c2e181e470dcd79d8ee50855c0686d615
Ghost0159 <contact.ghost01@gmail.com>
2023-01-25 03:39:52 +0100

    Adding screenshot

--- Nintendo eShop DB/download_games_xml.py
+++ Nintendo eShop DB/download_games_xml.py
@@ -42,9 +42,8 @@ for country_code in country_codes:
         if product_code.startswith('CTR') or product_code.startswith('KTR'):
             # Create directory to store XML files
             game_directory = f"Nintendo eShop DB/{country_code}/{format_name(name)}"
-            if os.path.exists(game_directory):
-                continue
-            os.makedirs(game_directory)
+            if not os.path.exists(game_directory):
+                os.makedirs(game_directory)
             # Save XML file as game_info.xml
             xml_file = f"{game_directory}/game_info.xml"
             with open(xml_file, "w", encoding="utf-8") as f:
@@ -61,8 +60,32 @@ for country_code in country_codes:
                 # convert to png
                 img = Image.open(icon_file)
                 img.save(icon_file)
-                
-                #resize the image to 48x48
+
+                # resize the image to 48x48
                 img = Image.open(icon_file)
                 img = img.resize((48, 48))
                 img.save(f"{game_directory}/icon_48x48.png")
+
+            # Create directory to store screenshots
+            screenshots_directory = f"{game_directory}/screenshots"
+            if not os.path.exists(screenshots_directory):
+                os.makedirs(screenshots_directory)
+
+            # download and combine screenshots
+            screenshots_element = content.find(".//screenshots")
+            if screenshots_element is not None:
+                screenshot_count = 1
+                for screenshot in screenshots_element.findall(".//screenshot"):
+                    upper_image_url = screenshot.find(".//image_url[@type='upper']")
+                    lower_image_url = screenshot.find(".//image_url[@type='lower']")
+                    if upper_image_url is not None and lower_image_url is not None:
+                        upper_image_url = upper_image_url.text
+                        upper_image = Image.open(BytesIO(requests.get(upper_image_url).content))
+                        lower_image_url = lower_image_url.text
+                        lower_image = Image.open(BytesIO(requests.get(lower_image_url).content))
+                        total_height = upper_image.height + lower_image.height
+                        combined_image = Image.new("RGBA", (upper_image.width, total_height))
+                        combined_image.paste(upper_image, (0, 0))
+                        combined_image.paste(lower_image, (0, upper_image.height))
+                        combined_image.save(f"{screenshots_directory}/screen_{screenshot_count}.png", "PNG")
+                        screenshot_count += 1
