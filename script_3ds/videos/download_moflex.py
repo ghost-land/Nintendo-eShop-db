db5632cdf0da57a460d2f1c1a2f1b15416ba4f42
Ghost0159 <contact.ghost01@gmail.com>
2023-12-04 23:37:53 +0100

    translate comments of download moflex files python script

--- script_3ds/videos/download_moflex.py
+++ script_3ds/videos/download_moflex.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from urllib.parse import urlparse
 from concurrent.futures import ThreadPoolExecutor
 
-MAX_WORKERS = 30  # Nombre maximum de threads à utiliser
+MAX_WORKERS = 30  # Maximum number of threads to use
 
 links_req = requests.get("""https://web.archive.org/cdx/search/cdx?url=https://kanzashi-movie-ctr.cdn.nintendo.net/m/&matchType=prefix&filter=statuscode%3A200&filter=original:.*(.moflex).*&fl=original&collapse=urlkey""")
 links = links_req.text.splitlines()
@@ -13,7 +13,7 @@ if not os.path.exists("moflex"):
     os.mkdir("moflex")
 os.chdir("moflex")
 
-# Fonction pour télécharger un fichier
+# Function to download a file
 def download_file(url):
     parsed_url = urlparse(url)
     file_name = os.path.basename(parsed_url.path)
@@ -23,13 +23,13 @@ def download_file(url):
     progress_bar = tqdm(total=total_size, unit="iB", unit_scale=True)
     with open(file_name, "wb") as file:
         for data in response.iter_content(chunk_size=1024):
-            # Mettre à jour la taille des données téléchargées
+            # Update downloaded data size
             progress_bar.update(len(data))
-            # Écrire les données dans le fichier
+            # Write data to file
             file.write(data)
     progress_bar.close()
 
-# Utiliser ThreadPoolExecutor pour télécharger les fichiers en parallèle
+# Use ThreadPoolExecutor to download files in parallel
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
     for link in links:
         url = "http://web.archive.org/web/20220220115806/"+link
